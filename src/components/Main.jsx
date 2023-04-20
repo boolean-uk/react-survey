147e6e08f285c968266ab9e27062a7133901446b
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-20 11:11:49 -0400

    finished the form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,7 +23,7 @@ function Main() {
       <section className="main__form">
         <form className= "form" on Submit={handleSubmit} >
         <h2>Tell us what you think about your rubber duck!</h2>
-          <div class="form__group radio"></div>
+          <div class="form__group radio">
             <h3>How do you rate your rubber duck colour?</h3>
               <RadioInput 
               label = '1'
@@ -64,7 +64,71 @@ function Main() {
               checkedOption={formData.form__group_radio}
               onChange={handleChange}
               />
+              </div>
 
+              <div class="form__group">
+              <h3>How do you like to spend time with your rubber duck</h3>
+              <CheckBox
+              label = 'swimming'
+              name = 'spend-time'
+              type = 'checkbox' 
+              value={formData.form__group}
+              onChange={handleChange}
+              />
+
+              <CheckBox
+              label = 'bathing'
+              name = 'spend-time'
+              type = 'checkbox' 
+              value={formData.form__group}
+              onChange={handleChange}
+              />
+
+              <CheckBox
+              label = 'chatting'
+              name = 'spend-time'
+              type = 'checkbox' 
+              value={formData.form__group}
+              onChange={handleChange}
+              />
+
+              <CheckBox
+              label = 'I dont like to spend time with it'
+              name = 'spend-time'
+              type = 'checkbox' 
+              value={formData.form__group}
+              onChange={handleChange}
+              />
+              </div>
+
+              <div class= 'review'>
+                <TextArea 
+                label = "What else have you got to say about your rubber duck?"
+                name= "review" 
+                cols= "30" 
+                rows= "10"
+                value = {formData.review}
+                onChange = {handleChange}
+                />
+
+              < div class='text'>
+                <Input 
+                label = 'Put your name here (if you feel like it):'
+                type = 'text'
+                name = 'username'
+                value = {formData.text}
+                onChange = {handleChange}
+              /> 
+              <Input
+              label = 'Leave us your email pretty please??'
+              type = 'email'
+              name = 'email'
+              value = {formData.email}
+              onChange = {handleChange}
+              />
+              </div>
+              <input type = "submit" 
+              value = "Submit!"/>
 
         </form>
       </section>
