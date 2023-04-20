3d9b090487a7c8dc87b041b2ed2187c6f3469b9c
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-20 16:25:30 +0200

    working on radio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,29 +3,31 @@ import { useState } from "react";
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
 
-  //const [color, setColor] = useState("")
+  const [color, setColor] = useState("");
   // const [time, setTime] = useState("")
-  const [review, setReview] = useState("")
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
+  const [review, setReview] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleColor = (e) => {
+    setColor(e.target.checked);
+    console.log(color);
+  };
 
   const handleEmail = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
   const handleName = (e) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
   const handleReview = (e) => {
-    setReview(e.target.value)
-  }
-
+    setReview(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("okkk")
-  }
-
-
+    e.preventDefault();
+    console.log("okkk");
+  };
 
   return (
     <main className="main">
@@ -33,94 +35,119 @@ function Main() {
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
-      <section className="main__form">{/* a form should be here */}
-      <form class="form" onSubmit={handleSubmit}>
-  <h2>Tell us what you think about your rubber duck!</h2>
-  <div class="form__group radio">
-    <h3>How do you rate your rubber duck colour?</h3>
-    <ul>
-  <li>
-    <input id="color-one" type="radio" name="color" value="1" /><label
-      for="color-one"
-      >1</label
-    >
-  </li>
-  <li>
-    <input id="color-two" type="radio" name="color" value="2" /><label
-      for="color-two"
-      >2</label
-    >
-  </li>
-  <li>
-    <input id="color-three" type="radio" name="color" value="3" /><label
-      for="color-three"
-      >3</label
-    >
-  </li>
-  <li>
-    <input id="color-four" type="radio" name="color" value="4" /><label
-      for="color-four"
-      >4</label
-    >
-  </li>
-</ul>
-
-  </div>
-  <div class="form__group">
-    <h3>How do you like to spend time with your rubber duck</h3>
-    <ul>
-  <li>
-    <label
-      ><input
-        name="spend-time"
-        type="checkbox"
-        value="swimming"
-      />Swimming</label
-    >
-  </li>
-  <li>
-    <label
-      ><input name="spend-time" type="checkbox" value="bathing" />Bathing</label
-    >
-  </li>
-  <li>
-    <label><input
-        name="spend-time"
-        type="checkbox"
-        value="chatting"
-      />Chatting</label
-    >
-  </li>
-  <li>
-    <label
-      ><input name="spend-time" type="checkbox" value="noTime" />I don't like to
-      spend time with it</label
-    >
-  </li>
-</ul>
-
-  </div>
-  <label
-    >What else have you got to say about your rubber duck?<textarea
-      name="review"
-      cols="30"
-      rows="10"
-      value={review} onChange={handleReview} 
-    ></textarea></label
-  ><label
-    >Put your name here (if you feel like it):<input
-      type="text"
-      name="username"
-      value={name} onChange={handleName} /></label
-  ><label
-    >Leave us your email pretty please??
-    <input
-      type="email"
-      name="email"
-      value={email} onChange={handleEmail} /></label
-  ><input class="form__submit" type="submit" value="Submit Survey!"/>
-</form>
-</section>
+      <section className="main__form">
+        {/* a form should be here */}
+        <form class="form" onSubmit={handleSubmit}>
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div class="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+              <li>
+                <input
+                  id="color-one"
+                  type="radio"
+                  name="color"
+                  value="1"
+                  onChange={handleColor}
+                  checked={color}
+                />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input
+                  id="color-two"
+                  type="radio"
+                  name="color"
+                  value="2"
+                  onChange={handleColor}
+                  checked={color}
+                />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input
+                  id="color-three"
+                  type="radio"
+                  name="color"
+                  value="3"
+                  onChange={handleColor}
+                  checked={color}
+                />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input
+                  id="color-four"
+                  type="radio"
+                  name="color"
+                  value="4"
+                  onChange={handleColor}
+                  checked={color}
+                />
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+          </div>
+          <div class="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="swimming" />
+                  Swimming
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="bathing" />
+                  Bathing
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="chatting" />
+                  Chatting
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input name="spend-time" type="checkbox" value="noTime" />I
+                  don't like to spend time with it
+                </label>
+              </li>
+            </ul>
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea
+              name="review"
+              cols="30"
+              rows="10"
+              value={review}
+              onChange={handleReview}
+            ></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input
+              type="text"
+              name="username"
+              value={name}
+              onChange={handleName}
+            />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+            />
+          </label>
+          <input class="form__submit" type="submit" value="Submit Survey!" />
+        </form>
+      </section>
     </main>
   );
 }
