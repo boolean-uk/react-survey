8ffd95fdd66f6e8fddae0476cf7eab6ff4ba5a64
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-20 18:05:49 +0300

    Some progress to start

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -1,13 +1,13 @@
 import AnswersItem from "./AnswersItem";
 
-export default function AnswersList(props) {
-  console.log("Inside AnswersList: ", props);
+export default function AnswersList({answers}) {
+  console.log("Inside AnswersList: ", answers);
 
-  const { answersList } = props;
+  const {answersList} = answers;
 
   return (
     <ul>
-      {answersList.map((answerItem, i) => (
+      {answers.map((answerItem, i) => (
         <AnswersItem answerItem={answerItem} key={i} />
       ))}
     </ul>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,59 @@
 import { useState } from "react";
+import AnswersList from "./AnswersList";
+import Checkboxes from "./Checkboxes";
+import RadioButtons from "./RadioButtons";
+
+const initialForm = {
+  color: '',
+  spendTime: '',
+  review: '',
+  username: '',
+  email: ''
+}
+
+const initialAnswers = [initialForm]
 
 function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [formData , setFormData] = useState(initialForm)
+  const [answers, setAnswers] = useState(initialAnswers)
 
   return (
     <main className="main">
       <section className={`main__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
-        {/* answers should go here */}
+        <AnswersList answers={answers} />
       </section>
-      <section className="main__form">{/* a form should be here */}</section>
+      <section className="main__form">
+        <form className="form">
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div className="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <RadioButtons />
+          </div>
+          <div className="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <Checkboxes />
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea name="review" cols="30" rows="10"></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input type="text" name="username" value="" />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input type="email" name="email" value="" />
+          </label>
+          <input
+            className="form__submit"
+            type="submit"
+            value="Submit Survey!"
+          />
+        </form>
+      </section>
     </main>
   );
 }
