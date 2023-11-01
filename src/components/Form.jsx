cf620bb1563b118046de942f570e90ce56b6effa
Satoki <satokiito@gmail.com>
2023-11-01 15:42:59 +0000

    core - set of radio buttons added to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,35 +1,49 @@
 function Form() {
-
-    return (
-        <form className="form">
-            <h2>Tell us what you think about your rubber duck!</h2>
-            <div className="form__group radio">
-              <h3>How do you rate your rubber duck colour?</h3>
-              {/* <!-- Radio inputs go here --> */}
-            </div>
-            <div className="form__group">
-              <h3>How do you like to spend time with your rubber duck</h3>
-              {/* <!-- checkboxes go here --> */}
-            </div>
-            <label>
-              What else have you got to say about your rubber duck?
-              <textarea name="review" cols="30" rows="10"></textarea>
-            </label>
-            <label>
-              Put your name here (if you feel like it):
-              <input type="text" name="username" value="" />
-            </label>
-            <label>
-              Leave us your email pretty please??
-              <input type="email" name="email" value="" />
-            </label>
-            <input
-              className="form__submit"
-              type="submit"
-              value="Submit Survey!"
-            />
-          </form>
-    )
+  return (
+    <form className="form">
+      <h2>Tell us what you think about your rubber duck!</h2>
+      <div className="form__group radio">
+        <h3>How do you rate your rubber duck colour?</h3>
+        {
+          <ul>
+            <li>
+              <input id="color-one" type="radio" name="color" value="1" />
+              <label htmlFor="color-one">1</label>
+            </li>
+            <li>
+              <input id="color-two" type="radio" name="color" value="2" />
+              <label htmlFor="color-two">2</label>
+            </li>
+            <li>
+              <input id="color-three" type="radio" name="color" value="3" />
+              <label htmlFor="color-three">3</label>
+            </li>
+            <li>
+              <input id="color-four" type="radio" name="color" value="4" />
+              <label htmlFor="color-four">4</label>
+            </li>
+          </ul>
+        }
+      </div>
+      <div className="form__group">
+        <h3>How do you like to spend time with your rubber duck</h3>
+        {/* <!-- checkboxes go here --> */}
+      </div>
+      <label>
+        What else have you got to say about your rubber duck?
+        <textarea name="review" cols="30" rows="10"></textarea>
+      </label>
+      <label>
+        Put your name here (if you feel like it):
+        <input type="text" name="username" value="" />
+      </label>
+      <label>
+        Leave us your email pretty please??
+        <input type="email" name="email" value="" />
+      </label>
+      <input className="form__submit" type="submit" value="Submit Survey!" />
+    </form>
+  );
 }
 
-export default Form
+export default Form;
