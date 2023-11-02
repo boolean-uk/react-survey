8e2d7e97a763c8daa12bc7c9bb5f283a3a292030
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-02 09:37:38 +0100

    removed console.logs

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -1,5 +1,4 @@
 function Checkbox ({value, description}) {
-  console.log(value, description)
   return (
     <label>
       <input
@@ -13,7 +12,6 @@ function Checkbox ({value, description}) {
 }
 
 export default function Checkboxes({valDescArr}) {
-  // valDescArr.forEach((elementArr, index) => console.log(index, elementArr[0], elementArr[1]))
   return (
     valDescArr.map((elementArr, index) => <Checkbox key={index} value={elementArr[1]} description={elementArr[0]}/>)
   )
