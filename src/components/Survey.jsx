783f96dd2a7f519168a776e6f3ed7d2e311a25a1
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-26 14:38:14 +0100

    Finished core

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
+import Form from "./Form";
+
+const INITIAL_FORM_STATE = {
+  color: "",
+  timeSpent: [],
+  review: "",
+  name:"",
+  email:""
+}
 
 function Survey() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [formData, setFormData] = useState(INITIAL_FORM_STATE)
+  const reset = () =>{setFormData({...INITIAL_FORM_STATE})
+    console.log("Resetting form")
+
+  }
 
   return (
     <main className="survey">
@@ -9,7 +23,9 @@ function Survey() {
         <h2>Answers list</h2>
         {/* answers should go here */}
       </section>
-      <section className="survey__form">{/* a form should be here */}</section>
+      <section className="survey__form">
+        <Form setFormData={setFormData} formData={formData} reset = {reset}/>
+      </section>
     </main>
   );
 }
