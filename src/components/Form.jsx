f06d4c0b7c70f661f8c45380e1f3c92bfa6e5ba2
Matthias Greeven <matsgreeven@gmail.com>
2024-02-27 11:02:49 +0100

    finished core

---
+++ src/components/Form.jsx
@@ -0,0 +1,135 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+function Form(props) {
+    Form.propTypes = {
+        addAnswer: PropTypes.func
+    }
+    const [formData, setFormData] = useState({
+        color: 0,
+        timeSpent: [],
+        review: "",
+        name: "",
+        email: "",
+      });
+
+    const handleInput = (event) =>{
+        const {name, type, value, checked} = event.target;
+        if (name=== "color")
+        {
+            setFormData({...formData, color:value})
+        }
+        if (name ==="review"){
+            setFormData({...formData, review:value})
+        }
+        if (name=== "email"){
+            setFormData({...formData, email:value})
+        }
+        if (name ==="username"){
+            setFormData({...formData, name:value})
+        }
+        if (name==="spend-time"){
+            if (!formData.timeSpent.includes(value) && checked)
+            {
+                formData.timeSpent.push(value)
+                setFormData({...formData})
+    
+                //Add value to timespent array
+            }
+            else if (formData.timeSpent.includes(value) && !checked)
+            {
+                const index = formData.timeSpent.indexOf(value)
+                formData.timeSpent.splice(index, 1)
+                setFormData({...formData})
+                //Remove value from timespent array
+            }
+        }
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        props.addAnswer(formData); // Call your addAnswer function with the form data
+      };
+    
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+        <h2>Tell us what you think about your rubber duck!</h2>
+        
+        <div className="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+            <li>
+                <input id="color-one" type="radio" name="color" value="1" onClick={handleInput} />
+                <label htmlFor="color-one">1</label>
+            </li>
+            
+            <li>
+                <input id="color-two" type="radio" name="color" value="2" onClick={handleInput}/>
+                <label htmlFor="color-two">2</label>
+            </li>
+            
+            <li>
+                <input id="color-three" type="radio" name="color" value="3" onClick={handleInput}/>
+                <label htmlFor="color-three">3</label>
+            </li>
+            
+            <li>
+                <input id="color-four" type="radio" name="color" value="4" onClick={handleInput}/>
+                <label htmlFor="color-four">4</label>
+            </li>
+            </ul>
+        </div>
+        
+        <div className="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+            <li>
+                <label>
+                <input name="spend-time" type="checkbox" value="swimming" onClick={handleInput}/>
+                Swimming
+                </label>
+            </li>
+            
+            <li>
+                <label>
+                <input name="spend-time" type="checkbox" value="bathing" onClick={handleInput}/>
+                Bathing
+                </label>
+            </li>
+            
+            <li>
+                <label>
+                <input name="spend-time" type="checkbox" value="chatting" onClick={handleInput}/>
+                Chatting
+                </label>
+            </li>
+            
+            <li>
+                <label>
+                <input name="spend-time" type="checkbox" value="noTime" onClick={handleInput} />
+                I don't like to spend time with it
+                </label>
+            </li>
+            </ul>
+        </div>
+        
+        <label>
+            What else have you got to say about your rubber duck?
+            <textarea name="review" cols="30" rows="10" onChange={handleInput}></textarea>
+        </label>
+        
+        <label>
+            Put your name here (if you feel like it):
+            <input type="text" name="username" value={formData.name} onChange={handleInput}/>
+        </label>
+        
+        <label>
+            Leave us your email pretty please??
+            <input type="email" name="email" value={formData.email} onChange={handleInput}/>
+        </label>
+        
+        <input className="form__submit" type="submit" value="Submit Survey!"/>
+    </form>
+  )
+}
+
+export default Form
--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
+import Form from "./Form";
+import AnswersList from "./AnswersList";
 
 function Survey() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [answers, setAnswers] = useState([])
+
+  const addAnswer = (answer) => {
+    answers.push({answer})
+    console.log(answers)
+  }
+  
 
   return (
     <main className="survey">
       <section className={`survey__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
         {/* answers should go here */}
+        {/* <AnswersList answerList={answers} /> */}
       </section>
-      <section className="survey__form">{/* a form should be here */}</section>
+      <section className="survey__form">
+        <Form addAnswer ={addAnswer}/>
+      </section>
     </main>
   );
 }
