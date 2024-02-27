7ccd541d10752f3c599993834aef2d0aaa30088e
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-27 16:03:49 +0100

    Made color rate multiple answer dynamic.

--- src/components/Form/MultipleAnswers.jsx
+++ src/components/Form/MultipleAnswers.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 export const MultipleAnswers = ({ option, setForm }) => {
+  const ratingScale = [1, 2, 3, 4];
+
   const handleClick = (value) => {
     setForm(prevForm => ({...prevForm, colorRating: value}));
   }
@@ -7,51 +9,19 @@ export const MultipleAnswers = ({ option, setForm }) => {
   return (
     <div className="form__group radio">
       <h3>How do you rate your rubber duck colour?</h3>
-
-        <li>
-          <input
-            id="color-one"
-            type="radio"
-            name="color"
-            value="1"
-            checked={option === 1}
-            onChange={() => handleClick(1)}
-          />
-          <label htmlFor="color-one">1</label>
-        </li>
-        <li>
-          <input
-            id="color-two"
-            type="radio"
-            name="color"
-            value="2"
-            checked={option === 2}
-            onChange={() => handleClick(2)}
-          />
-          <label htmlFor="color-two">2</label>
-        </li>
-        <li>
-          <input
-            id="color-three"
-            type="radio"
-            name="color"
-            value="3"
-            checked={option === 3}
-            onChange={() => handleClick(3)}
-          />
-          <label htmlFor="color-three">3</label>
-        </li>
-        <li>
-          <input
-            id="color-four"
-            type="radio"
-            name="color"
-            value="4"
-            checked={option === 4}
-            onChange={() => handleClick(4)}
-          />
-          <label htmlFor="color-four">4</label>
-        </li>
+        {ratingScale.map((rating) => (
+          <li key={rating}>
+            <input
+              id={`color-${rating}`}
+              type="radio"
+              name="color"
+              value={rating}
+              checked={option === rating}
+              onChange={() => handleClick(rating)}
+            />
+            <label htmlFor={`color-${rating}`}>{rating}</label>
+          </li>
+        ))}
     </div>
   );
 }
