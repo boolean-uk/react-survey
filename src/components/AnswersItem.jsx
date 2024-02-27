bb995030641159818cf4f046af1d73a74db7d6c0
Kristian <70452674+Solido7@users.noreply.github.com>
2024-02-27 13:10:46 +0100

    core + extensions

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 // Components don't need to be separeted into individual files
 // Here we have a smaller component that helps compose the AnswersItem below
 
@@ -8,25 +9,31 @@ const answersSet = {
   noTime: "I don't like to spend time with it"
 };
 
-function ItemsList({ list }) {
+const ItemsList = ({ list }) => {
   return (
     <ul>
-      {list.map((item) => (
-        <li>{answersSet[item]}</li>
+      {list.map((item, i) => (
+        <li key={i}>{answersSet[item]}</li>
       ))}
     </ul>
   );
 }
 
+ItemsList.propTypes = {
+  list: PropTypes.array,
+};
+
 // This is the main component being exported from this file
 export default function AnswersItem({
   // Feel free to change this props names to what suits you best
   // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
+  answerItem: { username, colour, timeSpent, review }, i, setEdit, deleteAnswer
 }) {
   return (
     <li>
       <article className="answer">
+        <button onClick={() => setEdit(old => ({isEditing: !old.isEditing, index: i}))}>Edit</button>
+        <button onClick={deleteAnswer}>Delete</button>
         <h3>{username || "Anon"} said:</h3>
         <p>
           <em>How do you rate your rubber duck colour?</em>
@@ -44,3 +51,10 @@ export default function AnswersItem({
     </li>
   );
 }
+
+AnswersItem.propTypes = {
+  answerItem: PropTypes.object,
+  i: PropTypes.number,
+  setEdit: PropTypes.func,
+  deleteAnswer: PropTypes.func,
+};
