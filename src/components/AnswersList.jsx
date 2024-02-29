7eb5ff9e2b3fce0ee91688932aaa3cb21dc6f0b2
Espen Solhaug <espen.solhaug@no.experis.com>
2024-02-29 15:51:48 +0100

    Core & Extension 1+3 finished

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -3,11 +3,11 @@ import AnswersItem from "./AnswersItem";
 export default function AnswersList(props) {
   console.log("Inside AnswersList: ", props);
 
-  const { answersList } = props;
+  const { answerList } = props;
 
   return (
     <ul>
-      {answersList.map((answerItem, i) => (
+      {answerList.map((answerItem, i) => (
         <AnswersItem answerItem={answerItem} key={i} />
       ))}
     </ul>
