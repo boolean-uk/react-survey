5bedd9e0022ad0c98cfb55726a348a9f09b13a59
pkekkonen <paulina.kekkonen@hotmail.com>
2024-02-29 15:11:11 +0100

    Ext 1 completed

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
+import AnswersList from "./AnswersList";
 
 const radioButtonValues = [1, 2, 3, 4];
-const bestOrWorstThings = ["It's yellow!", "It squeaks!", "It has a logo!", "It's big!"];
-const activities = ["Swimming", "Bathing", "Chatting", "I don't like spending time with it"];
+const bestOrWorstThings = [
+  "It's yellow!",
+  "It squeaks!",
+  "It has a logo!",
+  "It's big!",
+];
+const activities = [
+  "Swimming",
+  "Bathing",
+  "Chatting",
+  "I don't like spending time with it",
+];
+
+const initialUserData = {
+  username: "",
+  colour: "",
+  timeSpent: [],
+  review: "",
+  bestThings: [],
+  worstThings: [],
+  consistency: "",
+  logo: "",
+};
 
 function Survey() {
   const [open, setOpen] = useState(false); //Ignore this state
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState(initialUserData);
+  const [answers, setAnswers] = useState([]);
 
   function handleChange(event) {
     const inputName = event.target.name;
@@ -16,28 +39,19 @@ function Survey() {
     if (event.target.type !== "checkbox") {
       setUserData({ ...userData, [inputName]: inputValue });
     } else {
+      //TODO: why doesnt thiw work the second time i try too submit a form
       if (inputChecked) {
-        if (userData[inputName] === undefined) {
-          setUserData({ ...userData, [inputName]: [inputValue] });
-        } else {
-          setUserData({
-            ...userData,
-            [inputName]: [...userData[inputName], inputValue],
-          });
-        }
+        setUserData({
+          ...userData,
+          [inputName]: [...userData[inputName], inputValue],
+        });
       } else {
-        if (userData[inputName].length === 1) {
-          const updateUserData = userData;
-          delete updateUserData[inputName];
-          setUserData(updateUserData);
-        } else {
-          setUserData({
-            ...userData,
-            [inputName]: userData[inputName].filter(
-              (data) => data !== inputValue
-            ),
-          });
-        }
+        setUserData({
+          ...userData,
+          [inputName]: userData[inputName].filter(
+            (data) => data !== inputValue
+          ),
+        });
       }
     }
   }
@@ -49,15 +63,16 @@ function Survey() {
     //   method: "POST",
     //   body: JSON.stringify(userData),
     // });
+    setAnswers([...answers, userData]);
     event.target.reset();
-    setUserData({});
+    setUserData(initialUserData);
   }
 
   return (
     <main className="survey" onSubmit={handleSubmit}>
       <section className={`survey__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
-        {/* answers should go here */}
+        <AnswersList answersList={answers} />
       </section>
       <section className="survey__form">
         <form className="form">
@@ -70,25 +85,25 @@ function Survey() {
               <label key={index}>
                 <input
                   type="checkbox"
-                  name="best-things"
+                  name="bestThings"
                   value={value}
                   onChange={handleChange}
+                  checked={userData.bestThings.includes(value)}
                 />
                 {value}
               </label>
             ))}
           </div>
           <div className="form__group">
-            <h3>
-              What would you say are the worst bits of your rubber duck?
-            </h3>
+            <h3>What would you say are the worst bits of your rubber duck?</h3>
             {bestOrWorstThings.map((value, index) => (
               <label key={index}>
                 <input
                   type="checkbox"
-                  name="worst-things"
+                  name="worstThings"
                   value={value}
                   onChange={handleChange}
+                  checked={userData.worstThings.includes(value)}
                 />
                 {value}
               </label>
@@ -119,12 +134,12 @@ function Survey() {
                 <li key={index}>
                   <input
                     type="radio"
-                    name="color"
+                    name="colour"
                     value={value}
-                    id={"color" + value}
+                    id={"colour" + value}
                     onChange={handleChange}
                   />
-                  <label key={index} htmlFor={"color" + value}>
+                  <label key={index} htmlFor={"colour" + value}>
                     {value}
                   </label>
                 </li>
@@ -154,9 +169,11 @@ function Survey() {
               <label key={index}>
                 <input
                   type="checkbox"
-                  name="activity"
+                  name="timeSpent"
                   value={value}
                   onChange={handleChange}
+                  checked={userData.timeSpent.includes(value)}
+
                 />
                 {value}
               </label>
@@ -164,7 +181,12 @@ function Survey() {
           </div>
           <label>
             What else have you got to say about your rubber duck?
-            <textarea name="review" cols="30" rows="10"></textarea>
+            <textarea
+              name="review"
+              cols="30"
+              rows="10"
+              onChange={handleChange}
+            ></textarea>
           </label>
           <label>
             Put your name here (if you feel like it):
