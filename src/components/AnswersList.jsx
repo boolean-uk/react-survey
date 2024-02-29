212a5969cc01712d790ec26f1f7bf6d1a5220777
Enock97 <enockladu@hotmail.com>
2024-02-29 15:04:08 +0100

    "Completed core, and extension 1 and 2 exercises"

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -3,12 +3,17 @@ import AnswersItem from "./AnswersItem";
 export default function AnswersList(props) {
   console.log("Inside AnswersList: ", props);
 
-  const { answersList } = props;
+  const { answersList, onEdit } = props;
 
   return (
     <ul>
-      {answersList.map((answerItem, i) => (
-        <AnswersItem answerItem={answerItem} key={i} />
+      {answersList.map((answersItem, index) => (
+        <AnswersItem
+          key={index}
+          index={index}
+          answerItem={answersItem}
+          onEdit={onEdit}
+        />
       ))}
     </ul>
   );
