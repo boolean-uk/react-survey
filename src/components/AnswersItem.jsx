7ab05ad60b7105f67d9c2f4b988247f81372a510
Alexander <alexander.ellnestam@gmail.com>
2024-03-01 08:47:19 +0100

    Can delete and edit

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -1,5 +1,4 @@
-// Components don't need to be separeted into individual files
-// Here we have a smaller component that helps compose the AnswersItem below
+import PropTypes from "prop-types";
 
 const answersSet = {
   swimming: "Swimming",
@@ -11,8 +10,8 @@ const answersSet = {
 function ItemsList({ list }) {
   return (
     <ul>
-      {list.map((item) => (
-        <li>{answersSet[item]}</li>
+      {list.map((item, index) => (
+        <li key={index}>{answersSet[item]}</li>
       ))}
     </ul>
   );
@@ -22,15 +21,26 @@ function ItemsList({ list }) {
 export default function AnswersItem({
   // Feel free to change this props names to what suits you best
   // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
-}) {
+  answerItem: { username, color, timeSpent, review, email }, index, editForm, deleteForm
+}) 
+{
+  const handleEdit = () =>
+  {
+    editForm({username, color, timeSpent, review, email, index}) 
+  }
+
+  const handleDelete = () =>
+  {
+    deleteForm({index})
+  }
+  
   return (
     <li>
       <article className="answer">
         <h3>{username || "Anon"} said:</h3>
         <p>
           <em>How do you rate your rubber duck colour?</em>
-          <span className="answer__line">{colour}</span>
+          <span className="answer__line">{color}</span>
         </p>
         <p>
           <em>How do you like to spend time with your rubber duck?</em>
@@ -40,7 +50,16 @@ export default function AnswersItem({
           <em>What else have you got to say about your rubber duck?</em>
           <span className="answer__line">{review}</span>
         </p>
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
       </article>
     </li>
   );
 }
+
+AnswersItem.propTypes = {
+  answerItem: PropTypes.object,
+  index: PropTypes.number,
+  editForm: PropTypes.func,
+  deleteForm: PropTypes.func
+};
--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,157 +1,222 @@
 import { useState } from "react";
+import AnswersList from "./AnswersList";
+
+let answerForms = [];
 
 function Survey() {
+  const [open, setOpen] = useState(false);
 
-  const InitialState = {
-    duckRating: "",
-    duckActivitySwimming: false,
-    duckActivityBathing: false,
-    duckActivityChatting: false,
-    duckActivitNoTime: false,
-    otherDuckComments: "",
-    userName: "",
-    userEmail: "",
+  const initialData = {
+    color: "",
+    timeSpent: [],
+    review: "",
+    username: "",
+    email: "",
+  };
+  const [data, setData] = useState(initialData);
+  const [answerData, setAnswerData] = useState(answerForms);
+  const [changes, setChanges] = useState(-1);
 
-  }
+  const submit = (event) => {
+    event.preventDefault();
+    answerForms.push(data);
+    setAnswerData(answerForms);
+    setData(initialData);
+  };
 
-  const [open, setOpen] = useState(false); //Ignore this state
+  const editSubmit = (event) => {
+    event.preventDefault();
+    answerForms[changes] = data;
+    setAnswerData([...answerForms]);
+    setData(initialData);
+    setChanges(-1);
+  };
 
-  const [surveyData, setSurveyData] = useState(InitialState)
+  const input = (event) => {
+    const { name, type, value, checked } = event.target;
 
-  function duckSurveyInput(event) {
-    const inputName = event.target.name
-    const inputType = event.target.type
-    const inputValue = event.target.value
-    const checked = event.target.checked
+    if (type === "checkbox") {
+      let arr = [...data.timeSpent];
+      if (checked) arr.push(value);
+      else arr.splice(arr.indexOf(value), 1);
+      setData({ ...data, timeSpent: arr });
+    } else setData({ ...data, [name]: value });
+  };
 
-    if (inputName === "color") {
-      setSurveyData({...surveyData, duckRating: inputValue})
-    }
-    
-    if (inputName === "spend-time" && inputType === "checkbox") {
-      if (inputValue === "swimming") {
-        setSurveyData({...surveyData, duckActivitySwimming: checked})
-      }
-    }
+  const editForm = (data) => {
+    setData({
+      color: data.color,
+      timeSpent: data.timeSpent,
+      review: data.review,
+      username: data.username,
+      email: data.email,
+    });
+    setChanges(data.index);
+  };
 
-    if (inputValue === "bathing") {
-      setSurveyData({...surveyData, duckActivityBathing: checked})
-    }
-
-    if (inputValue === "chatting") {
-      setSurveyData({...surveyData, duckActivityChatting: checked})
-    }
-
-    if (inputValue === "noTime") {
-      setSurveyData({...surveyData, duckActivitNoTime: checked})
-    }
-
-    if(inputName === "review") {
-      setSurveyData({...surveyData, otherDuckComments: inputValue})
-    }
-
-    if (inputName === "username") {
-      setSurveyData({...surveyData, userName: inputValue})
-    }
-
-    if (inputName === "email") {
-      setSurveyData({...surveyData, userEmail: inputValue})
-    }
-  }
-
-
-  function printSubmit(event) {
-    console.log(
-      `duckRating: ${surveyData.duckRating},
-      duckActivityChecks:
-      swimming:${surveyData.duckActivitySwimming},
-      bathing:${surveyData.duckActivityBathing},
-      chatting:${surveyData.duckActivityChatting},
-      noTime:${surveyData.duckActivityNoTime},
-      otherDuckComments: ${surveyData.otherDuckComments},
-      userName: ${surveyData.userName},
-      userEmail: ${surveyData.userEmail}`
-    )
-
-    event.preventDefault()
-    setSurveyData(InitialState)
-  }
+  const deleteForm = (data) => {
+    answerForms.splice(data.index, 1);
+    setAnswerData([...answerForms]);
+  };
 
   return (
-    <main className="survey">
-      <section className={`survey__list ${open ? "open" : ""}`}>
-        <h2>Answers list</h2>
-        {/* answers should go here */}
+    <>
+      <main className="survey">
+        <section className={`survey__list ${open ? "open" : ""}`}>
+          <h2>Answers list</h2>
+          {/* answers should go here */}
+          <AnswersList
+            answerData={answerData}
+            editForm={editForm}
+            deleteForm={deleteForm}
+          />
         </section>
-        <section className="survey-form">
-          <form className="form"> 
-            <h2>Tell us what you think about your rubber duck!</h2>
-            <div className="form-group radio">
-              <h3>How do you rate your rubber duck color?</h3>
-              <ul>
-                <li>
-                  <input id="color-one" type="radio" name="color" value="1" onChange={duckSurveyInput} checked={surveyData.duckRating === "1"} />
-                  <label htmlFor="color-one">1</label>
-                </li>
-                <li>
-                  <input id="color-two" type="radio" name="color" value="2" onChange={duckSurveyInput} checked={surveyData.duckRating === "2"} />
-                  <label htmlFor="color-two">2</label>
-                </li>
-                <li>
-                  <input id="color-three" type="radio" name="color" value="3" onChange={duckSurveyInput} checked={surveyData.duckRating === "3"} />
-                  <label htmlFor="color-three">3</label>
-                </li>
-                <li>
-                  <input id="color-four" type="radio" name="color" value="4" onChange={duckSurveyInput} checked={surveyData.duckRating === "4"} />
-                  <label htmlFor="color-four">4</label>
-                </li>
-              </ul>
-            </div>
-            <div className="form-group">
-              <h3>How do you like to spend time with your rubber duck</h3>
-              <ul>
-                <li>
-                  <label>
-                    <input name="spend-time" type="checkbox" value="swimming" onChange={duckSurveyInput} checked={surveyData.duckActivitySwimming}/>
-                    Swimming
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name="spend-time" type="checkbox" value="bathing" onChange={duckSurveyInput} checked={surveyData.duckActivitySwimming}/>
-                    Bathing
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name="spend-time" type="checkbox" value="chatting" onChange={duckSurveyInput} checked={surveyData.duckActivitySwimming}/>
-                    Chatting
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name="spend-time" type="checkbox" value="noTime" onChange={duckSurveyInput} checked={surveyData.duckActivitySwimming}/>
-                    I do not like to spend time wtih it
-                  </label>
-                </li>
-              </ul>
-            </div>
-            <label>
-              What else have you got to say about the rubber duck?
-              <textarea name="review" cols="30" rows="10" onChange={duckSurveyInput} value={surveyData.otherDuckComments}></textarea>
-            </label>
-            <label>
-              Put your name here (if you feel like it):
-              <input type="text" name="username" onChange={duckSurveyInput} value={surveyData.userName} />
-            </label>
-            <label>
-              Leave us your email pretty please??
-              <input type="email" name="email" onChange={duckSurveyInput} value={surveyData.userEmail} />
-            </label>
-            <input className="form-submit" type="submit" value="Submit Survey!" onClick={printSubmit}/>
-          </form>
-      </section>
-    </main>
+        <section className="survey__form">
+          {/* a form should be here */}
+        </section>
+        <form
+          className="form"
+          onSubmit={changes > -1 ? editSubmit : submit}
+        >
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div className="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+              <li>
+                <input
+                  id="color-one"
+                  type="radio"
+                  name="color"
+                  value="1"
+                  checked={data.color === "1"}
+                  onChange={input}
+                />
+                <label htmlFor="color-one">1</label>
+              </li>
+              <li>
+                <input
+                  id="color-two"
+                  type="radio"
+                  name="color"
+                  value="2"
+                  checked={data.color === "2"}
+                  onChange={input}
+                />
+                <label htmlFor="color-two">2</label>
+              </li>
+              <li>
+                <input
+                  id="color-three"
+                  type="radio"
+                  name="color"
+                  value="3"
+                  checked={data.color === "3"}
+                  onChange={input}
+                />
+                <label htmlFor="color-three">3</label>
+              </li>
+              <li>
+                <input
+                  id="color-four"
+                  type="radio"
+                  name="color"
+                  value="4"
+                  checked={data.color === "4"}
+                  onChange={input}
+                />
+                <label htmlFor="color-four">4</label>
+              </li>
+            </ul>
+          </div>
+          <div className="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+              <li>
+                <label>
+                  <input
+                    name="spend-time"
+                    type="checkbox"
+                    value="swimming"
+                    onChange={input}
+                    checked={data.timeSpent.includes("swimming")}
+                  />
+                  Swimming
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    name="spend-time"
+                    type="checkbox"
+                    value="bathing"
+                    onChange={input}
+                    checked={data.timeSpent.includes("bathing")}
+                  />
+                  Bathing
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    name="spend-time"
+                    type="checkbox"
+                    value="chatting"
+                    onChange={input}
+                    checked={data.timeSpent.includes("chatting")}
+                  />
+                  Chatting
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    name="spend-time"
+                    type="checkbox"
+                    value="noTime"
+                    onChange={input}
+                    checked={data.timeSpent.includes("noTime")}
+                  />
+                  I dont like to spend time with it
+                </label>
+              </li>
+            </ul>
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea
+              name="review"
+              cols="30"
+              rows="10"
+              value={data.review}
+              onChange={input}
+            ></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input
+              type="text"
+              name="username"
+              value={data.username}
+              onChange={input}
+            />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input
+              type="email"
+              name="email"
+              value={data.email}
+              onChange={input}
+            />
+          </label>
+          <input
+            className="form__submit"
+            type="submit"
+            value="Submit Survey!"
+          />
+        </form>
+      </main>
+    </>
   );
 }
 
