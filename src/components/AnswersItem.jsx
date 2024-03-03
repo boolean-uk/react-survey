cf03ab0a89aece665a4ee25a4b9ae810d40b1468
Ateeb <ateeb@live.no>
2024-03-03 01:14:29 +0100

    core and Extension

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -1,46 +1,71 @@
-// Components don't need to be separeted into individual files
-// Here we have a smaller component that helps compose the AnswersItem below
+import { useState } from "react";
+import ReactModal from 'react-modal'
+import EditAnswer from "./EditAnswer";
 
-const answersSet = {
-  swimming: "Swimming",
-  bathing: "Bathing",
-  chatting: "Chatting",
-  noTime: "I don't like to spend time with it"
-};
+// const answersSet = {
+//   swimming: "Swimming",
+//   bathing: "Bathing",
+//   chatting: "Chatting",
+//   noTime: "I don't like to spend time with it"
+// };
 
-function ItemsList({ list }) {
+function ItemsList(list) {
   return (
     <ul>
-      {list.map((item) => (
-        <li>{answersSet[item]}</li>
+      {list.list.map((item, index) => (
+        <li key={index}>{item}</li>
       ))}
     </ul>
   );
 }
 
+
 // This is the main component being exported from this file
-export default function AnswersItem({
-  // Feel free to change this props names to what suits you best
-  // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
-}) {
+export default function AnswersItem({answerItem: { username, color, spendTime = [], review }, setSubmissions}) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <li>
       <article className="answer">
         <h3>{username || "Anon"} said:</h3>
         <p>
           <em>How do you rate your rubber duck colour?</em>
-          <span className="answer__line">{colour}</span>
+          <span className="answer__line">{color}</span>
         </p>
         <p>
           <em>How do you like to spend time with your rubber duck?</em>
-          <ItemsList list={timeSpent} />
+          <ItemsList list={spendTime} />
         </p>
         <p>
           <em>What else have you got to say about your rubber duck?</em>
           <span className="answer__line">{review}</span>
         </p>
+        <button onClick={openModal}>Edit</button>
+        <ReactModal
+          className="reactBox"
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Edit Answer"
+        >
+          <EditAnswer 
+            closeModal={closeModal}
+            username={username}
+            color={color}
+            spendTime={spendTime}
+            review={review}
+            setSubmissions={setSubmissions} // This prop must be passed down from the parent
+          />
+        </ReactModal>
       </article>
     </li>
   );
 }
+
---
+++ src/components/EditAnswer.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import './edit.css'
+export default function EditAnswer({ closeModal, username, color, spendTime, review, submissionId, setSubmissions }) {
+  const [editedUsername, setEditedUsername] = useState(username);
+  const [editedColor, setEditedColor] = useState(color);
+  const [editedSpendTime, setEditedSpendTime] = useState(spendTime);
+  const [editedReview, setEditedReview] = useState(review);
+
+  const handleSave = () => {
+    // Update the submission with the new details
+    setSubmissions((prevSubmissions) =>
+      prevSubmissions.map((submission) =>
+        submission.id === submissionId
+          ? { ...submission, username: editedUsername, color: editedColor, spendTime: editedSpendTime, review: editedReview }
+          : submission
+      )
+    );
+    closeModal(); // Close the modal
+  };
+
+  return (
+    <div className="editWrap">
+      <div className="editForm">
+        <label>
+          Username:
+          <input
+            type="text"
+            value={editedUsername}
+            onChange={(e) => setEditedUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Color Rating:
+          <input
+            type="text"
+            value={editedColor}
+            onChange={(e) => setEditedColor(e.target.value)}
+          />
+        </label>
+        <label>
+          How do you like to spend time with your rubber duck?
+          <input
+            type="text"
+            value={editedSpendTime}
+            onChange={(e) => setEditedSpendTime(e.target.value)}
+          />
+        </label>
+        <label>
+          Review:
+          <textarea
+            value={editedReview}
+            onChange={(e) => setEditedReview(e.target.value)}
+          />
+        </label>
+        <button onClick={handleSave}>Save</button>
+        <button onClick={closeModal}>Close</button>
+      </div>
+
+    </div>
+  );
+}
