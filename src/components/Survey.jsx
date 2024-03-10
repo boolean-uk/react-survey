033b14ac2366aa101cb1dcb5e9bd7f13d797ef07
Mobeen <muhammadmobeen1@hotmail.com>
2024-03-10 06:55:02 +0100

    updated

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,131 +1,138 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState, useRef } from "react";
+import AnswersList from "./AnswersList";
 
-// 2. Create Initial State
-
-const INITIAL_STATE = {
-  color: "",
+const initialFormData = {
+  consistency: "",
+  colour: "",
+  logo: "",
+  timeSpent: [],
   review: "",
   username: "",
   email: "",
-  // time_spent: "",
 };
 
 function Survey() {
-  const [open, setOpen] = useState(false); //Ignore this state
+  const [open] = useState(false); //Ignore this state
+  const [formData, setFormData] = useState(initialFormData);
+  const [answers, setAnswers] = useState([]);
+  const [editMode, setEditMode] = useState(false)
+  const [editIndex, setEditIndex] = useState(null)
 
-  // 3. Common event listener callback function
-  const [form, setForm] = useState(INITIAL_STATE);
+  const formRef = useRef(null)
 
-  // 5. Submit the form
-
-  const submitForm = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    const options = {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(form),
-    };
-    fetch("http://localhost:5174", options);
-
-    // Log the data from form
-    console.log(form, "logging the data");
-
-    // setting Form
-    setForm(INITIAL_STATE);
+    if (editMode && editIndex !== null) {
+      const toUpdate = [...answers]
+      toUpdate[editIndex] = formData
+      setAnswers(toUpdate);
+      setEditMode(false)
+      setEditIndex(null)
+    } else {
+      setAnswers([...answers, formData])
+    }
+    console.log(formData)
+    setFormData(initialFormData);
+    formRef.current.reset()
   };
-
-  // 4. If statements: if the input type is a checkbox or radio type need an if statement because the value is either true or false
 
   const handleChange = (event) => {
-    const { name, type, value, checked } = event.target;
-    if (type === "checkbox") {
-      setForm({ ...form, [name]: checked ? value : "" });
-    } else {
-      setForm({ ...form, [name]: value });
-    }
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  console.log(form);
+  const handleCheck = (event) => {
+    const { name, checked } = event.target;
+    if (checked) {
+      setFormData({ ...formData, timeSpent: [...formData.timeSpent, name] });
+    } else {
+      setFormData({
+        ...formData,
+        timeSpent: formData.timeSpent.filter((item) => item !== name),
+      });
+    }
+  }
+
+  const handleEditClick = (index) => {
+    setFormData(answers[index])
+    setEditMode(true)
+    setEditIndex(index)
+  }
 
   return (
     <main className="survey">
       <section className={`survey__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
-        {/* answers should go here */}
+        <AnswersList answersList={answers} handleEditClick={handleEditClick} />
       </section>
+
       <section className="survey__form">
-        {/* a form should be here */}
-
-        {/* 1. FORM HERE - COPIED FROM TEMPLATE */}
-
-        <form className="form">
+        <form className="form" ref={formRef} onSubmit={handleSubmit}>
           <h2>Tell us what you think about your rubber duck!</h2>
-          <div className="form__group radio">
-            <h3>How do you rate your rubber duck colour?</h3>
-            {/* <!-- Radio inputs go here --> */}
 
-            {/* <!-- This is a radio buttons group --> */}
-            <ul>
-              <li>
+          <div className="form__group radio">
+            <h3>How do you rate your rubber duck consistency?</h3>
+            {["1", "2", "3", "4"].map((value) => (
+              <>
                 <input
-                  onChange={(event) => handleChange(event)}
-                  id="color-one"
+                  key={value}
+                  id={`colour${value}`}
+                  name="colour"
+                  value={value}
                   type="radio"
-                  name="color"
-                  value="1"
-                  checked={form.color === "1"}
+                  onChange={handleChange}
+                  checked={formData.colour === value}
                 />
-                <label htmlFor="color-one">1</label>
-              </li>
-              <li>
+                <label htmlFor={`colour${value}`}>{value}</label>
+              </>
+            ))}
+
+            <h3>How do you rate your rubber duck colour?</h3>
+            {["1", "2", "3", "4"].map((value) => (
+              <>
                 <input
-                  onChange={(event) => handleChange(event)}
-                  id="color-two"
+                  key={value}
+                  id={`consistency${value}`}
+                  name="consistency"
+                  value={value}
                   type="radio"
-                  name="color"
-                  value="2"
-                  checked={form.color === "2"}
+                  onChange={handleChange}
+                  checked={formData.consistency === value}
                 />
-                <label htmlFor="color-two">2</label>
-              </li>
-              <li>
+                <label htmlFor={`consistency${value}`}>{value}</label>
+              </>
+            ))}
+
+            <h3>How do you rate your rubber duck logo?</h3>
+            {["1", "2", "3", "4"].map((value) => (
+              <>
                 <input
-                  onChange={(event) => handleChange(event)}
-                  id="color-three"
+                  key={value}
+                  id={`logo${value}`}
+                  name="logo"
+                  value={value}
                   type="radio"
-                  name="color"
-                  value="3"
-                  checked={form.color === "3"}
+                  onChange={handleChange}
+                  checked={formData.logo === value}
                 />
-                <label htmlFor="color-three">3</label>
-              </li>
-              <li>
-                <input
-                  onChange={(event) => handleChange(event)}
-                  id="color-four"
-                  type="radio"
-                  name="color"
-                  value="4"
-                  checked={form.color === "4"}
-                />
-                <label htmlFor="color-four">4</label>
-              </li>
-            </ul>
+                <label htmlFor={`logo${value}`}>{value}</label>
+              </>
+            ))}
           </div>
+
           <div className="form__group">
             <h3>How do you like to spend time with your rubber duck</h3>
-            {/* <!-- checkboxes go here --> */}
-            {/* <!-- This is a checkboxes group --> */}
+
             <ul>
               <li>
                 <label>
                   <input
-                    onChange={(event) => handleChange(event)}
-                    name="time_spent"
+                    name="swimming"
                     type="checkbox"
                     value="swimming"
-                    checked={form.time_spent === "swimming"}
+                    onChange={handleCheck}
                   />
                   Swimming
                 </label>
@@ -133,11 +140,10 @@ function Survey() {
               <li>
                 <label>
                   <input
-                    onChange={(event) => handleChange(event)}
-                    name="time_spent"
+                    name="bathing"
                     type="checkbox"
                     value="bathing"
-                    checked={form.time_spent === "bathing"}
+                    onChange={handleCheck}
                   />
                   Bathing
                 </label>
@@ -145,11 +151,10 @@ function Survey() {
               <li>
                 <label>
                   <input
-                    onChange={(event) => handleChange(event)}
-                    name="time_spent"
+                    name="chatting"
                     type="checkbox"
                     value="chatting"
-                    checked={form.time_spent === "chatting"}
+                    onChange={handleCheck}
                   />
                   Chatting
                 </label>
@@ -157,50 +162,54 @@ function Survey() {
               <li>
                 <label>
                   <input
-                    onChange={(event) => handleChange(event)}
-                    name="time_spent"
+                    name="noTime"
                     type="checkbox"
                     value="noTime"
-                    checked={form.time_spent === "noTime"}
+                    onChange={handleCheck}
                   />
-                  I don't like to spend time with it
+                  I do not like to spend time with it
                 </label>
               </li>
             </ul>
+
+            <label>
+              What else have you got to say about your rubber duck?
+              <textarea
+                id="review"
+                name="review"
+                cols="30"
+                rows="10"
+                value={formData.review}
+                onChange={handleChange}
+              ></textarea>
+            </label>
+
+            <label>
+              Put your name here (if you feel like it):
+              <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </label>
+
+            <label>
+              Leave us your email pretty please??
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </label>
+
+            <input
+              className="form__submit"
+              type="submit"
+              value="Submit Survey!"
+            />
           </div>
-          <label>
-            What else have you got to say about your rubber duck?
-            <textarea
-              onChange={(event) => handleChange(event)}
-              name="review"
-              cols="30"
-              rows="10"
-              value={form.review}
-            ></textarea>
-          </label>
-          <label>
-            Put your name here (if you feel like it):
-            <input
-              onChange={(event) => handleChange(event)}
-              type="text"
-              name="username"
-              value={form.username}
-            />
-          </label>
-          <label>
-            Leave us your email pretty please??
-            <input
-              onChange={(event) => handleChange(event)}
-              type="email"
-              name="email"
-              value={form.email}
-            />
-          </label>
-          <input
-            className="form__submit"
-            type="submit"
-            value="Submit Survey!"
-          />
         </form>
       </section>
     </main>
